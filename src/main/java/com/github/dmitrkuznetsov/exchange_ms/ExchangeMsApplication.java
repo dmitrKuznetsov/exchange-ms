e9b5d8ca1c4b1903829359d18c30b80d33adec2a
dmitrKuznetsov <kuznetsov1995dima@mail.ru>
2023-02-21 23:26:24 +0300

    spring initializer

---
+++ src/main/java/com/github/dmitrkuznetsov/exchange_ms/ExchangeMsApplication.java
@@ -0,0 +1,13 @@
+package com.github.dmitrkuznetsov.exchange_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeMsApplication.class, args);
+	}
+
+}
