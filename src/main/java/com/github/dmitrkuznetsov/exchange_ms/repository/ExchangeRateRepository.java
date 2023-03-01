905c9f492e7da31121e2d114c5c697a3b4bd2686
dmitrKuznetsov <kuznetsov1995dima@mail.ru>
2023-03-01 16:49:38 +0300

    Added Postman file, Validation, Reverse set exchange ratio

--- src/main/java/com/github/dmitrkuznetsov/exchange_ms/repository/ExchangeRateRepository.java
+++ src/main/java/com/github/dmitrkuznetsov/exchange_ms/repository/ExchangeRateRepository.java
@@ -12,6 +12,5 @@ import java.util.Optional;
 @Repository
 public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, ExchangeRateId> {
   List<ExchangeRate> findAllByIdCurrencyFrom(Currency currencyFrom);
-
   Optional<ExchangeRate> findById(ExchangeRateId exchangeRateId);
 }
